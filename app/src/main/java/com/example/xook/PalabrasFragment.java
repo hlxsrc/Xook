e14be576820d89e90fbd25bdb27d7a769883705a
Hugo Rodriguez <alexisrodcal@gmail.com>
2019-03-09 11:59:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/xook/PalabrasFragment.java
@@ -0,0 +1,33 @@
+package com.example.xook;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PalabrasFragment extends Fragment {
+
+
+    public PalabrasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_palabras, container, false);
+    }
+
+}
+
